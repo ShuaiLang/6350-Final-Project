b0ac3a03f81f2138380cf962f591919af16bfaba
Shuai Lang <aspiringls942@gmail.com>
2020-04-08 15:32:09 -0700

    add take photo and image pick

--- lib/pages/addPost.dart
+++ lib/pages/addPost.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:final6350/model.dart';
 import 'package:validators/validators.dart' as validator;
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 
 class addPost extends StatefulWidget {
@@ -11,6 +18,8 @@ class addPost extends StatefulWidget {
 class _addPostState extends State<addPost> {
   final _formKey = GlobalKey<FormState>();
   Model model = Model();
+  final databaseReference = Firestore.instance;
+
   @override
   Widget build(BuildContext context) {
     final halfMediaWidth = MediaQuery.of(context).size.width / 2.0;
@@ -67,7 +76,7 @@ class _addPostState extends State<addPost> {
             ),
             MyTextFormField(
               hintText: 'description',
-              isDescription: true,
+//              isDescription: true,
               validator: (String value) {
                 if (value.isEmpty) {
                   return 'Please enter a description';
@@ -78,12 +87,63 @@ class _addPostState extends State<addPost> {
                 model.description = value;
               },
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton.icon(
+                  onPressed: () {
+                    if (model.imageList == null ||
+                        (model.imageList != null && model.imageList.length < 4)) {
+                      pickImage();
+                    } else {
+                      // show snack bar or sth?
+                    }
+                  },
+                  color: Colors.green,
+                  icon: Icon(
+                    Icons.photo,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Add image',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                RaisedButton.icon(
+                  onPressed: () {
+                    if (model.imageList == null ||
+                        (model.imageList != null && model.imageList.length < 4)) {
+                      takePhoto();
+                    } else {
+                      // show snack bar or sth?
+                    }
+                  },
+                  color: Colors.green,
+                  icon: Icon(
+                    Icons.camera_enhance,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Take photo',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            // show picked images
+            MultiImagePickerList(
+              imageList: model.imageList,
+              removeNewImage: (index) {
+                removeImage(index);
+                return null;
+              }),
             RaisedButton(
               color: Colors.blueAccent,
-              onPressed: () {
+              onPressed: () async {
                 if (_formKey.currentState.validate()) {
                   _formKey.currentState.save();
-
+                  await postInfo(databaseReference, model.title, model.price,
+                      model.description, model.imageList);
                   Navigator.pop(context, '${model.title} added!');
                 }
               },
@@ -93,12 +153,128 @@ class _addPostState extends State<addPost> {
                   color: Colors.white,
                 ),
               ),
-            )
+            ),
+            // not figure out how to make it to the most buttom
           ],
         ),
       ),
     );
   }
+  takePhoto() async {
+    File file = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (file != null) {
+      //imagesMap[imagesMap.length] = file;
+      List<File> imageFile = new List();
+      // imageList = new List.from(imageFile);
+      imageFile.add(file);
+      if (model.imageList == null) {
+        model.imageList = new List.from(imageFile, growable: true);
+      } else {
+        for (int s = 0; s < imageFile.length; s++) {
+          model.imageList.add(file);
+        }
+      }
+//      setState(() {});
+    }
+  }
+
+  pickImage() async {
+    File file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      //imagesMap[imagesMap.length] = file;
+      List<File> imageFile = new List();
+      // imageList = new List.from(imageFile);
+      imageFile.add(file);
+      if (model.imageList == null) {
+        model.imageList = new List.from(imageFile, growable: true);
+      } else {
+        for (int s = 0; s < imageFile.length; s++) {
+          model.imageList.add(file);
+        }
+      }
+//      setState(() {});
+    }
+  }
+
+  removeImage(int index) async {
+    //imagesMap.remove(index);
+    model.imageList.removeAt(index);
+//    setState(() {});
+  }
+
+  void postInfo(databaseReference, title, price, description, imageList) async {
+    // post image successful then post other info
+    List<String> randomFileNames = [];
+
+    for (var image in imageList) {
+      // var completePath = image.path;
+      // var fileName = (completePath.split('/').last);
+      // print(completePath + ' ' +fileName);
+
+      // generate a random file name
+      String fileName = UniqueKey().toString() + '.jpg';
+      randomFileNames.add(fileName);
+      StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child(fileName);
+      StorageUploadTask uploadTask = firebaseStorageRef.putFile(image);
+      StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+    }
+
+    await databaseReference.collection('post').add({
+      'title': title,
+      'price': price,
+      'description': description,
+      'images': randomFileNames,
+    });
+  }
+}
+
+Widget MultiImagePickerList(
+    {List<File> imageList, VoidCallback removeNewImage(int position)}) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+    child: imageList == null || imageList.length == 0
+        ? Container()
+        : SizedBox(
+      height: 150.0,
+      child: ListView.builder(
+          itemCount: imageList.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.only(left: 3.0, right: 3.0),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.withAlpha(100),
+                        borderRadius:
+                        BorderRadius.all(Radius.circular(15.0)),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: FileImage(imageList[index]))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.red[600],
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.clear,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            removeNewImage(index);
+                          }),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }),
+    ),
+  );
 }
 
 class MyTextFormField extends StatelessWidget {
